9c1fb1b042f0994b2bf08c9ce0a9f785f272537c
vketipisz <vketipisz@qmetric.co.uk>
2016-04-11 16:09:23 +0100

    using random client references

--- src/main/groovy/com/qmetric/tonto/StubLoanRangerService.groovy
+++ src/main/groovy/com/qmetric/tonto/StubLoanRangerService.groovy
@@ -3,10 +3,30 @@ package com.qmetric.tonto
 import groovy.json.JsonSlurper
 import spark.Spark
 
+import java.security.SecureRandom
+
 import static spark.Spark.post
 
 
 class StubLoanRangerService {
+
+    private static final int ID_LENGTH = 16;
+
+    private static final char[] POSSIBLE_ID_CHARS = "abcdefghijklmnopqrstuvwxyz_0123456789".toCharArray()
+
+    static def generateClientReference()
+    {
+        final StringBuilder id = new StringBuilder();
+
+        final Random random = new SecureRandom();
+
+        for (int charIndex = 0; charIndex < ID_LENGTH; charIndex++)
+        {
+            id.append(POSSIBLE_ID_CHARS[random.nextInt(POSSIBLE_ID_CHARS.length)]);
+        }
+
+        return id.toString();
+    }
 
     static def registerEndpoints() {
         Spark.get("/ping", { request, response ->
@@ -24,11 +44,11 @@ class StubLoanRangerService {
             def accountNumber = object.bankAccount.accountNumber as String
 
             if(accountNumber == "99999999") {
-                '''{"status":"REJECTED","clientReference":"ffffffffffffffff","errorCode":"CCD_Declined","message":"There is a problem with this transaction. Please transfer to the Customer Service team"}'''
+                '''{"status":"REJECTED","clientReference":"''' + generateClientReference() + '''","errorCode":"CCD_Declined","message":"There is a problem with this transaction. Please transfer to the Customer Service team"}'''
             } else if(accountNumber == "88888888") {
-                '''{"status":"PENDING","clientReference":"ffffffffffffffff","message":"Transaction is undergoing a review for CCD checks"}'''
+                '''{"status":"PENDING","clientReference":"''' + generateClientReference() + '''","message":"Transaction is undergoing a review for CCD checks"}'''
             } else {
-                '''{"status": "SUCCESS","clientReference": "ffffffffffffffff","loanReference": "WEGAVEHIMEMONEY01"}'''
+                '''{"status": "SUCCESS","clientReference": "''' + generateClientReference() + '''","loanReference": "WEGAVEHIMEMONEY01"}'''
             }
         })
     }
